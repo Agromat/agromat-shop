e063af5e01988a6dbb595ad8460c5adbbbba207b
Eugene Godun <eugene.godun@gmail.com>
2015-07-15 16:22:16 +0300

    submenu

--- js/common.js
+++ js/common.js
@@ -14,6 +14,31 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
+	$(".js-nav a").on("click", function(){
+		var parent = $(this).parent();
+		var index = +parent.index()+1;
+		$('[data-index="'+index+'"]').toggleClass("is-visible");
+		parent.toggleClass("is-open");
+		$(".js-overlay").toggleClass("is-visible");
+	});
+	var timeout;
+	$(".js-overlay").hover(
+		function() {	
+			var self = $(this);
+			timeout = setTimeout(function(){
+				$(".js-subnav").removeClass("is-visible");
+				$(".js-nav li").removeClass("is-open");
+				self.removeClass("is-visible");
+			}, 1000);			
+		}, function() {
+			clearTimeout(timeout);
+		}
+	);
+	$(".js-overlay").on("click", function(){
+		$(".js-subnav").removeClass("is-visible");
+		$(".js-nav li").removeClass("is-open");
+		$(this).removeClass("is-visible");
+	});
 
-	console.log($('body').html());
+
 });
